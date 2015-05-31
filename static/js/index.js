e9b6017c937db017bdf61d65fb4d420e5d990410
Dan <djphan@gmail.com>
2015-05-30 21:11:18 -0600

    Added Scrolling Animation and Buttons. Lower Quality of Parallax image to improve perfomrance.

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,40 @@
+function rmNavColor(element) {
+	var numNavBarClasses = element.className.split('').length;
+	for (i=0; i<numNavBarClasses; i++) {
+		$('#navbar').removeClass(element.className.split(' ').pop());
+	}
+	return;
+}
+
 $( document ).ready(function() {
- 
-	$('.parallax-window').parallax({imageSrc: 'static/img/headerimg.jpg'});
- 
+	// Navbar Color Changing
+	var navid = 0;
+	var a = $("#navbar").offset().top;
+
+	$('#navbar').addClass('blue-grey darken-2');
+
+	$(document).scroll (function() {
+
+	});
+    
+ 	// Parallax Setup
+ 	$('parallax-window').parallax({
+ 		speed: '0.3',
+		bleed: '10' 
+ 	});
+
+ 	// Link to each window setup
+	$('#parallax1').parallax({
+		imageSrc: 'static/img/headerimg.jpg',
+	});
+
+	// Add Scrolling Transitions
+	var $root = $('html, body');
+	$('a').click(function() {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
 });
