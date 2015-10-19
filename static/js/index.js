0897e0578eae72abdae889bf5689fe95b5b5e237
Dan <djphan@gmail.com>
2015-10-18 18:12:12 -0600

    Starting removal of parallax.js. Cleanup to use MarteralizeCSS. Allows for more stability for parallax windows when resizing on internet browsers.

--- static/js/index.js
+++ static/js/index.js
@@ -14,6 +14,7 @@ function goToSlide(element, slide) {
 // Document Setup Block
 $( document ).ready(function() {
 
+
 	// Standardize Text Size
 	$('.content').css("font-size", "12.5pt");
 	$('.content2').css("font-size", "14pt");
@@ -22,18 +23,7 @@ $( document ).ready(function() {
 	$(".button-collapse").sideNav();
 
  	// Parallax Setup
- 	$('parallax-window').parallax({
- 		speed: '0.3',
-		bleed: '10',
-		iosFix: true,
- 	});
-
- 	// Link to each window setup
-	$('#parallax1').parallax({ imageSrc: 'static/img/headerimg.jpg', });
-	$('#parallax2').parallax({ imageSrc: 'static/img/gel.jpg', });
-	$('#parallax3').parallax({ imageSrc: 'static/img/gel.jpg', });
-	$('#parallaxf').parallax({ imageSrc: 'static/img/footerimg.png	', });
-
+	$('.parallax').parallax();
 
 	// Add Scrolling Transitions
 	var $root = $('html, body');
@@ -49,7 +39,7 @@ $( document ).ready(function() {
 	$('.skills_slider').slick();
 	$('.workHistorySlider').slick();
 
-	// Carousel Content. Automatically make alternating lists look notice
+	// Carousel Content. Automatically make alternating lists look nice
 	$(".altern li:odd").prepend($('<i class="orange-text mdi-navigation-chevron-right"></i>'));
 	$(".altern li:even").prepend($('<i class="red-text mdi-navigation-chevron-right"></i>'));
 
@@ -59,12 +49,3 @@ $( document ).ready(function() {
 	$('.links li').prepend($('<i class="blue-text mdi-navigation-chevron-right"></i>'));
 
 });
-
-
-function rmNavColor(element) {
-	var numNavBarClasses = element.className.split('').length;
-	for (i=0; i<numNavBarClasses; i++) {
-		$('#navbar').removeClass(element.className.split(' ').pop());
-	}
-	return;
-}
