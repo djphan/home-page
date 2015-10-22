eebbfe5d6fa40bd211b54446438698aa10f76403
djphan <djphan@gmail.com>
2015-10-22 01:41:08 -0600

    Cleaned up parallax so it scales better when resizing. Cleaned up content and images.

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,24 @@
 // Helper functions to command the slick Carousel
-function nextSlide(element) {
+function nextSlide (element) {
 	$(element).slick('slickNext');
 }
 
-function prevSlide(element) {
+function prevSlide (element) {
 	$(element).slick('slickPrev');
 }
 
-function goToSlide(element, slide) {
+function goToSlide (element, slide) {
 	$(element).slick('slickGoTo', slide);
 }
 
 // Document Setup Block
-$( document ).ready(function() {
-
-
+$(document).ready(function () {
 	// Standardize Text Size
-	$('.content').css("font-size", "12.5pt");
-	$('.content2').css("font-size", "14pt");
+	$('.content').css('font-size', '12.5pt');
+	$('.content2').css('font-size', '14pt');
 
 	// Navbar Setup
-	$(".button-collapse").sideNav();
+	$('.button-collapse').sideNav();
 
  	// Parallax Setup
 	$('.parallax').parallax();
@@ -40,12 +38,13 @@ $( document ).ready(function() {
 	$('.workHistorySlider').slick();
 
 	// Carousel Content. Automatically make alternating lists look nice
-	$(".altern li:odd").prepend($('<i class="orange-text mdi-navigation-chevron-right"></i>'));
-	$(".altern li:even").prepend($('<i class="red-text mdi-navigation-chevron-right"></i>'));
-
+	$('.altern li:odd').prepend($('<i class=\'orange-text mdi-navigation-chevron-right\'></i>'));
+	$('.altern li:even').prepend($('<i class=\'red-text mdi-navigation-chevron-right\'></i>'));
 	$('.altern li:odd span').addClass('orange-text');
 	$('.altern li:even span').addClass('red-text');
 
-	$('.links li').prepend($('<i class="blue-text mdi-navigation-chevron-right"></i>'));
+	$('.links li').prepend($('<i class=\'blue-text mdi-navigation-chevron-right\'></i>'));
 
+	// Fixing spacing for H3 elements
+	$('h3').wrapInner('<span class=\"col m7 s10 offset-s1"></span>');
 });
